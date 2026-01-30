1ea2aa7dd879ae760dfb15c49497b4af6c3146f3
longevidadeativa60-debug <longevidadeativa60@gmail.com>
2026-01-30 20:33:32 -0300

    Add files via upload

---
+++ meu_drive/app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, render_template, request, send_from_directory
+
+app = Flask(__name__)
+
+# Configura onde os arquivos serão salvos
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    # Lista todos os arquivos da pasta uploads
+    arquivos = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('drive.html', arquivos=arquivos)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "Nenhum arquivo encontrado", 400
+    file = request.files['file']
+    if file.filename == '':
+        return "Nenhum arquivo selecionado", 400
+    
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return "OK", 200
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
